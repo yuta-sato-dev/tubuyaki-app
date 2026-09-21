5e50153ac08d0236be32d4014b7b3cde55b4ef72
Yuta Sato <yuta.sato.dev+github@gmail.com>
2026-09-21 17:47:31 +0900

    feat: ルートを定義

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/sample', [\App\Http\Controllers\sample\IndexController::class, 'show']);
+Route::get('/sample/{id}', [\App\Http\Controllers\sample\IndexController::class, 'showId']);
+Route::get('/post', \App\Http\Controllers\Post\IndexController::class);
