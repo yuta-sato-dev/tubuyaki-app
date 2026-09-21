34d4cddec396eef82bec396e29ad6ff0b09954cd
Yuta Sato <yuta.sato.dev+github@gmail.com>
2026-09-21 18:25:04 +0900

    feat: テストようにpostsテーブルのSeederを作成する

---
+++ database/seeders/PostsSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class PostsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('posts')->insert([
+            'content' => Str::random(100),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+    }
+}
