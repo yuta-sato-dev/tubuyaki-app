3db316a7dc2f01aa619f412e4810f889d08b2b69
Yuta Sato <yuta.sato.dev+github@gmail.com>
2026-09-22 16:16:03 +0900

    feat: content用のダミーテキストを生成するPostFactoryを追加

--- database/seeders/PostsSeeder.php
+++ database/seeders/PostsSeeder.php
@@ -2,10 +2,11 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Post;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Str;
+// use Illuminate\Support\Facades\DB;
+// use Illuminate\Support\Str;
 
 class PostsSeeder extends Seeder
 {
@@ -14,10 +15,11 @@ class PostsSeeder extends Seeder
      */
     public function run(): void
     {
-        DB::table('posts')->insert([
-            'content' => Str::random(100),
-            'created_at' => now(),
-            'updated_at' => now(),
-        ]);
+        Post::factory()->count(10)->create();
+        // DB::table('posts')->insert([
+            // 'content' => Str::random(100),
+            // 'created_at' => now(),
+            // 'updated_at' => now(),
+        // ]);
     }
 }
